e47d08a29d03b33b37679914950e7dda333c1415
Estefania-Catano <ecatano1@cesde.net>
2025-05-07 18:57:22 -0500

    Examen final

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        String opcion="";
+        String dia=""
+        System.out.println("Menú de opciones");
+        System.out.println("1. Almuerzo, 2. Cena, 3. Reserver evento");
+        System.out.println("Escoga entre las opciones");
+        switch (opcion){
+            case "1":
+                System.out.println("Escoja un día de la semana");
+                System.out.println("L-Lunes, M-Martes, Mi-Miercoles, J-Jueves, V-Viernes, S-Sabado, D-Domingo");
+                switch (dia){
+                    case "L":
+                        System.out.println("Ingrese el valor de la cuenta");
+                }
+        }
+    }
+}
